87937f8ba3ff4c4f8e6025a70b29a0fd0f992319
desktop-gdy <rhdek228@gmail.com>
2023-11-29 00:52:15 +0900

    등록/수정/조회 API 만들기2+테스트코드 작성

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,9 +2,11 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 스프링부트의 자동설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정 (뭔솔)
 // 이 위치부터 설정을 읽어감 -> 항상 프로젝트의 최상담에 위치해야함
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor // final 붙은 멤버변수 인자값으로 생성자 자동 생성
 @RestController
@@ -15,5 +15,15 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
